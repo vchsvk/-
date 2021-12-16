e6daf3189d448ff25da632c1e5fe66d980f72d92
vchsvk <94437141+vchsvk@users.noreply.github.com>
2021-12-16 15:03:11 +0300

    Add files via upload

---
+++ malakhov 17/malakhov 17/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace malakhov_17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                int k = Convert.ToInt32(textBox2.Text);
+                textBox1.Text = Convert.ToString(k + 50);
+            }
+            if (radioButton2.Checked)
+            {
+                int k = Convert.ToInt32(textBox2.Text);
+                textBox1.Text = Convert.ToString(k + 100);
+            }
+            if (radioButton3.Checked)
+            {
+                int k = Convert.ToInt32(textBox2.Text);
+                textBox1.Text = Convert.ToString(k + 200);
+            }
+        }
+    }
+}
